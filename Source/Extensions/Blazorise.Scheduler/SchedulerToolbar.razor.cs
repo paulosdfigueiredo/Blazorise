8af7c4fbf44c6eb8648059ef75f73ac810e271ee
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-01-30 12:32:50 +0100

    Basic structure

---
+++ Source/Extensions/Blazorise.Scheduler/SchedulerToolbar.razor.cs
@@ -0,0 +1,36 @@
+﻿#region Using directives
+using Blazorise.Utilities;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.Scheduler;
+
+public partial class SchedulerToolbar : BaseComponent
+{
+    #region Members
+
+    #endregion
+
+    #region Methods
+
+    override protected void BuildClasses( ClassBuilder builder )
+    {
+        builder.Append( "b-scheduler-toolbar" );
+
+        base.BuildClasses( builder );
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the content to be rendered inside the component.
+    /// </summary>
+    /// <remarks>
+    /// This property allows developers to define custom content within the <see cref="SchedulerToolbar"/> component.
+    /// </remarks>
+    [Parameter] public RenderFragment ChildContent { get; set; }
+
+    #endregion
+}
